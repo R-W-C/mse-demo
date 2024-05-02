02b6ba1aa4bf0d6aaa599e787ef262717c88a326
R-W-C <36850866+R-W-C@users.noreply.github.com>
2024-05-02 15:09:51 +0200

    Update mse.js

--- js/mse.js
+++ js/mse.js
@@ -2,21 +2,21 @@
 * mse.js
 *
 * Code to play video with Media Source Extensions (MSE) API
-*
-* Based on code found here:
+* 
+* Based on code found here: 
 * https://github.com/bitmovin/mse-demo/blob/main/index.html
-*
-* For a good explanation, see
+* 
+* For a good explanation on the MSE API, see 
 * https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/samples/dn551368(v=vs.85)?redirectedfrom=MSDN
-*
+* 
 * Command line command used to create segments with a segment timeline:
-*
-* MP4Box -dash 10000 -segment-name part_  -url-template "massage in aandacht-merged (720p + audio).mp4" -rap -segment-timeline
-*
+* 
+* MP4Box -dash 10000 -segment-name part_  -url-template "original.mp4" -rap -segment-timeline
+* 
 * Output:
 * 1 mp4-file ("part_init.mp4")
-* 10 m4s-files ("part_0.m4s", "part_154112.m4s", .. , "part_1159168.m4s")
-* 1 mpd-file ("massage in aandacht-merged (720p + audio)_dash.mpd")
+* 10 m4s-files ("part_0.m4s", "part_153856.m4s" .. "part_1158912.m4s")
+* 1 mpd-file ("original_dash.mpd")
 *
 *************************************************************************************************************/
 
@@ -36,11 +36,7 @@
 	  "part_1025792.m4s",
 	  "part_1158912.m4s"
   ];
-
-
-
-
-
+	
   const BASEURL = './video/';
 
   const MIMETYPE = 'video/mp4';
